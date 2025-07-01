2e0ccedc74689da72780b85bdb101de04711bb06
Ortwin Carstocea <OrtwinC@ppedv.de>
2025-07-01 10:56:11 +0200

    Lab 006 Loesung

---
+++ Lab6_OOP/Fahrzeug.cs
@@ -0,0 +1,73 @@
+﻿namespace Lab6_OOP
+{
+    public class Fahrzeug
+    {
+        //Felder
+        private bool _motorLäuft = false;
+        private int _aktGeschwindigkeit = 0;
+        
+        //Properties
+        public string Name { get; set; }
+        public int MaxGeschwindigkeit { get; set; }
+        public double Preis { get; set; }
+
+        //Konstruktor mit Übergabeparametern und Standartwerten
+        public Fahrzeug(string name, int maxG, double preis)
+        {
+            Name = name;
+            MaxGeschwindigkeit = maxG;
+            Preis = preis;
+        }
+
+        //Methode zur Ausgabe von Objektinformationen
+        public string Info()
+        {
+            if (_motorLäuft)
+                return $"{Name} kostet {Preis}€ und fährt momentan mit {_aktGeschwindigkeit} von maximal {MaxGeschwindigkeit}km/h.";
+            else
+                return $"{Name} kostet {Preis}€ und könnte maximal {MaxGeschwindigkeit}km/h fahren.";
+        }
+
+        //Methode zum Starten des Motors
+        public void StarteMotor()
+        {
+            if (_motorLäuft)
+                Console.WriteLine($"Der Motor von {Name} läuft bereits.");
+            else
+            {
+                _motorLäuft = true;
+                Console.WriteLine($"Der Motor von {Name} wurde gestartet.");
+            }
+        }
+
+        //Methode zum Stoppen des Motors
+        public void StoppeMotor()
+        {
+            if (!_motorLäuft)
+                Console.WriteLine($"Der Motor von {Name} ist bereits gestoppt");
+            else if (_aktGeschwindigkeit > 0)
+                Console.WriteLine($"Der Motor kann nicht gestoppt werden, da sich {Name} noch bewegt");
+            else
+            {
+                _motorLäuft = false;
+                Console.WriteLine($"Der Motor von {Name} wurde gestoppt.");
+            }
+        }
+
+        //Methode zum Beschleunigen und Bremsen
+        public void Beschleunige(int a)
+        {
+            if (_motorLäuft)
+            {
+                if (_aktGeschwindigkeit + a > MaxGeschwindigkeit)
+                    _aktGeschwindigkeit = MaxGeschwindigkeit;
+                else if (_aktGeschwindigkeit + a < 0)
+                    _aktGeschwindigkeit = 0;
+                else
+                    _aktGeschwindigkeit += a;
+
+                Console.WriteLine($"{Name} bewegt sich jetzt mit {_aktGeschwindigkeit}km/h");
+            }
+        }
+    }
+}
---
+++ Lab6_OOP/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace Lab6_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ändern des durch Console verwendeten Zeichensatzes auf Unicode (damit das €-Zeichen angezeigt werden kann)
+            Console.OutputEncoding = Encoding.Unicode;
+
+            //Deklaration einer Fahrzeug-Variablen und Initialisierung mittels einer Fahrzeug-Instanz
+            Fahrzeug mercedes = new Fahrzeug("Mercedes", 190, 23000);
+            //Ausführen der Info()-Methode des Fahrzeugs und Ausgabe in der Konsole
+            Console.WriteLine(mercedes.Info() + "\n");
+
+            //Diverse Methodenausführungen
+            mercedes.StarteMotor();
+            mercedes.Beschleunige(120);
+            Console.WriteLine(mercedes.Info() + "\n");
+
+            mercedes.Beschleunige(300);
+            Console.WriteLine(mercedes.Info() + "\n");
+
+            mercedes.StoppeMotor();
+            Console.WriteLine(mercedes.Info() + "\n");
+
+            mercedes.Beschleunige(-500);
+            mercedes.StoppeMotor();
+            Console.WriteLine(mercedes.Info() + "\n");
+        }
+    }
+}
