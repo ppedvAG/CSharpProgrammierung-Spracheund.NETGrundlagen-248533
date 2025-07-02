49613d9a0f2c7fcdd82cc0bae1e9bf08426f1974
Ortwin Carstocea <OrtwinC@ppedv.de>
2025-07-02 14:51:45 +0200

    Module 014 WPF Applikation

---
+++ FahrzeugLib/Generator.cs
@@ -0,0 +1,36 @@
+﻿using Bogus;
+using Lab9_CountTypes.Fahrzeugpark;
+
+namespace FahrzeugLib
+{
+    public class Generator
+    {
+        private readonly Faker faker = new Faker()
+        {
+            // Wir setzen einen Seed, um immer die gleichen Zufallsdaten zu erhalten
+            Random = new Randomizer(42)
+        };
+
+        public FahrzeugBase[] GetFahrzeuge(int anzahl)
+        {
+            FahrzeugBase[] fahrzeuge = new FahrzeugBase[anzahl];
+
+            for (int i = 0; i < anzahl; i++)
+            {
+                fahrzeuge[i] = GenerateFahrzeug();
+            }
+
+            return fahrzeuge;
+        }
+
+        public Auto GenerateFahrzeug()
+        {
+            var name = $"{faker.Vehicle.Manufacturer()} {faker.Vehicle.Model()}";
+            var topSpeed = faker.Random.Int(50, 200);
+            var price = faker.Random.Int(10000, 50000);
+            var doors = faker.Random.Int(2, 5);
+
+            return new Auto(name, topSpeed, price, doors);
+        }
+    }
+}
---
+++ FahrzeugWPF/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using FahrzeugLib;
+using Lab9_CountTypes.Fahrzeugpark;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace FahrzeugWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Generator Generator = new Generator();
+
+        public ObservableCollection<FahrzeugBase> Fahrzeuge { get; } = new ObservableCollection<FahrzeugBase>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // DataContext wird auf die eigene Instanz gesetzt
+            DataContext = this;
+
+            foreach (FahrzeugBase f in Generator.GetFahrzeuge(5))
+            {
+                Fahrzeuge.Add(f);
+            }
+        }
+
+        private void btnAddFahrzeug_Click(object sender, RoutedEventArgs e)
+        {
+            Fahrzeuge.Add(Generator.GenerateFahrzeug());
+        }
+
+        private void btnDeleteFahrzeug_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstFahrzeuge.SelectedIndex > -1)
+            {
+                Fahrzeuge.RemoveAt(lstFahrzeuge.SelectedIndex);
+            }
+        }
+
+        private void lstFahrzeuge_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (lstFahrzeuge.SelectedIndex > -1)
+            {
+                txtPreis.Text = Fahrzeuge[lstFahrzeuge.SelectedIndex].Preis.ToString() + " Euro";
+            } 
+            else
+            {
+                txtPreis.Text = "";
+            }
+        }
+    }
+}
