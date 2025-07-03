8bb261df2f89991210069503c854bcb265dca3b7
Ortwin Carstocea <OrtwinC@ppedv.de>
2025-07-03 10:25:47 +0200

    Quiz Game - Arrays und Schleifen

--- QuizGame/Program.cs
+++ QuizGame/Program.cs
@@ -16,37 +16,64 @@
 
             Console.WriteLine($"Hallo, {name}!");
 
-            AskQuestion("Was ist die Hauptstadt von Deutschland?", "Berlin");
-            AskQuestion("Wie viele Beine hat eine Spinne?", "8");
+            AskQuestion("Was ist die Hauptstadt von Deutschland?", "Berlin", "Wien", "Frankfurt", "Muenchen", "Berlin");
+            AskQuestion("Wie viele Beine hat eine Spinne?", "8", "6", "8", "4", "12");
 
             Console.WriteLine($"Du hast {score} Punkte erreicht.");
-
-
 
             Console.WriteLine("Beliebige Taste zum Beenden druecken...");
             Console.ReadKey();
         }
 
-        private static string? AskQuestion(string question, string expected)
+        // Multiple Choice: Wir fuegen die Antwortmoeglichkeiten als Parameter hinzu von 0 bis n
+        private static void AskQuestion(string question, string expected, params string[] options)
         {
+            // Leerzeile hinzufuegen
+            Console.WriteLine();
             Console.WriteLine(question);
-            string? antwort = Console.ReadLine();
-            // Es kann eine NullReferenceException auftreten, weswegen wir einen Null-Check durchfuehren
-            // Equals von der Klasse string hat Ueberladungen, die nuetzlich sind
-            // InvariantCulture: Unabhaengigkeit von der eingestellten Sprache des Betriebsystems
-            // IgnoreCase: Groß- und Kleinschreibung wird ignoriert
-            if (antwort is not null && antwort.Equals(expected, StringComparison.InvariantCultureIgnoreCase))
+
+            // Mehrere Antworten moeglich
+            for (int i = 0; i < options.Length; i++)
             {
-                Console.WriteLine("Richtig!");
-                // Punktzahl um 1 erhoehen
-                score++;
+                Console.WriteLine($"\t({i + 1})\t{options[i]}");
+            }
+
+            string? input = Console.ReadLine();
+
+            // Werte konvertieren bzw. parsen
+            if (int.TryParse(input, out int number))
+            {
+                // Potentielle IndexOutOfRangeException: Wir muessen pruefen, ob der index innerhalb des Wertebereiches liegt
+                int index = number - 1;
+                if (index >= 0 && index < options.Length)
+                {
+                    string antwort = options[index];
+
+                    // Es kann eine NullReferenceException auftreten, weswegen wir einen Null-Check durchfuehren
+                    // Equals von der Klasse string hat Ueberladungen, die nuetzlich sind
+                    // InvariantCulture: Unabhaengigkeit von der eingestellten Sprache des Betriebsystems
+                    // IgnoreCase: Groß- und Kleinschreibung wird ignoriert
+                    if (antwort is not null && antwort.Equals(expected, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        Console.WriteLine("Richtig!");
+                        // Punktzahl um 1 erhoehen
+                        score++;
+
+                        // Funktion hier verlassen ohne Rueckgabe, weil void
+                        return;
+                    }
+
+                    Console.WriteLine("Leider falsch.");
+                }
+                else
+                {
+                    Console.WriteLine("Diese Option steht nicht zur Verfügung!");
+                }
             }
             else
             {
-                Console.WriteLine("Leider falsch.");
+                Console.WriteLine("Ungültige Eingabe!");
             }
-
-            return antwort;
         }
     }
 }
